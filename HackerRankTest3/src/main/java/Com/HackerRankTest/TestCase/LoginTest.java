d85ce559e8b538f884dc247a864f1655a0ff1e5d
Raheel <Raheel@DESKTOP-U6EFAKA>
2020-05-21 16:36:23 +0500

    Ramdom Data added

--- HackerRankTest3/src/main/java/Com/HackerRankTest/TestCase/LoginTest.java
+++ HackerRankTest3/src/main/java/Com/HackerRankTest/TestCase/LoginTest.java
@@ -1,13 +1,54 @@
 package Com.HackerRankTest.TestCase;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
 
 import Com.HackerRankTest.PageObject.LoginPage;
 
 
 public class LoginTest extends BaseClass {
+	
+	public void updateproFile(String value, String Case, String status) throws IOException {
+
+		// create object of faker which was added into POM.xml
+		Faker faker = new Faker();
+
+		// to Generate random data for test case process
+		String Firstname = faker.name().firstName();
+		String Password = faker.name().lastName();
+		
+		String Emailaddress = Firstname  + "@gmail.com";
+		
+		// create object for read and write RandomData properties file as input and output stream
+		FileOutputStream fileOut = null;
+        FileInputStream fileIn = null;
+        
+        // create object of properties class for set key and value into the properties file 
+            Properties configProperty = new Properties();
+  
+            // load RandomData properties for update and create key values
+            File file = new File("./configs/RandomData2.properties");
+            fileIn = new FileInputStream(file);
+            configProperty.load(fileIn);
+            configProperty.setProperty("Firstname", Firstname);
+    		configProperty.setProperty("Password", Password);
+    		configProperty.setProperty("Emailaddress", Emailaddress);
+    		
+            fileOut = new FileOutputStream(file);
+            configProperty.store(fileOut, null);
+            fileIn.close();
+           
+
+	}
 	
 	@Parameters({ "url1" })
 	@Test
